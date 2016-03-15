9ada7d948e182a3c38fc648058ab5fd4b5a7d2e6
Brendon Hollingsworth <tucaeg@yahoo.com>
2016-03-14 21:06:47 -0700

    Fixed Formatting

--- homework4.js
+++ homework4.js
@@ -1,35 +1,39 @@
-$(document).ready(function() {
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+function loadActors() {
   "use strict";
-
-
-  loadActors();
-
-  $("#add-button").click(function(){
-    $.post("http://localhost:3000/actors",{"name":$("#new-actor").val(), "starred":"false"},function(data) {
-      loadActors();
+  $(".demo-list-action").empty();
+  $.get("http://localhost:3000/actors", function(data) {
+    $.each(data, function(index, actor) {
+      var isStarred;
+      if (actor.starred === true) {
+        isStarred = "star";
+      } else {
+        isStarred = "star_border";
+      }
+      $(".demo-list-action").append(
+        "<div class=\"mdl-list__item\"> <span class=\"mdl-list__item-primary-content\"> <i class=\"material-icons mdl-list__item-avatar\">person</i> <span>" + actor.name + "</span> </span> <a class=\"mdl-list__item-secondary-action\" href=\"#\" onclick=\"starToggle(this)\" id=\"star" + actor.id + "\"><i class=\"material-icons\">" + isStarred + "</i></a> </div>"
+      );
     });
   });
+}
 
-
-
-
-
-});
 function starToggle(starInfo) {
-  var id=starInfo.id.slice(-1);
+  "use strict";
+  var id = starInfo.id.slice(-1);
   var actorData;
-  $.get("http://localhost:3000/actors/"+id, function (data) {
-    if (data.starred === true)
+  $.get("http://localhost:3000/actors/" + id, function(data) {
+    if (data.starred === true) {
       data.starred = false;
-    else
+    } else {
       data.starred = true;
+    }
     actorData = JSON.stringify(data);
     $.ajax({
-      url: 'http://localhost:3000/actors/'+id,
-      type: 'PUT',
-      contentType: 'application/json',
+      url: "http://localhost:3000/actors/" + id,
+      type: "PUT",
+      contentType: "application/json",
       data: actorData,
-      success: function(data){
+      success: function() {
         loadActors();
       }
     });
@@ -37,18 +41,23 @@ function starToggle(starInfo) {
 
 }
 
-function loadActors() {
-  $(".demo-list-action").empty();
-  $.get("http://localhost:3000/actors", function (data) {
-    $.each(data, function (index, actor) {
-      var isStarred;
-      if (actor.starred === true)
-        isStarred = "star";
-      else
-        isStarred = "star_border";
-      $(".demo-list-action").append(
-        '<div class="mdl-list__item"> <span class="mdl-list__item-primary-content"> <i class="material-icons mdl-list__item-avatar">person</i> <span>' + actor.name + '</span> </span> <a class="mdl-list__item-secondary-action" href="#" onclick="starToggle(this)" id="star'+ actor.id + '"><i class="material-icons">' + isStarred + '</i></a> </div>'
-      );
-    })
+
+
+$(document).ready(function() {
+  "use strict";
+
+  loadActors();
+
+  $("#add-button").click(function() {
+    $.post("http://localhost:3000/actors", {
+      "name": $("#new-actor").val(),
+      "starred": "false"
+    }, function() {
+      loadActors();
+    });
   });
-}
+
+
+
+
+});
